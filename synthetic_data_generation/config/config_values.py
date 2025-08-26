453ef8581684a6553778b3610197bf1b307304a4
ahmedheakl <ahmed.heakl@ejust.edu.eg>
2025-08-26 04:04:13 +0000

    Add hebrew configs

--- synthetic_data_generation/config/config_values.py
+++ synthetic_data_generation/config/config_values.py
@@ -1,8 +1,8 @@
 class ConfigValues:
 
     DATASET_PATH = "synthetic_data_generation/dataset/"
-    GENERATED_DATASET_PATH = "synthetic_data_generation/generated_latex/urdu/"
+    GENERATED_DATASET_PATH = "synthetic_data_generation/generated_latex/persian/"
     GENERATED_HF_DATASET_PATH = "synthetic_data_generation/generated_hf_dataset/"
-    TEMPLATES_PATH = "synthetic_data_generation/templates/templates/"
+    TEMPLATES_PATH = "synthetic_data_generation/templates/templates/persian/"
     HF_DATASET_FNAME_TRAILER = "_hf_data"
     HF_DATASET_PAGE_NUM_TRAILER = "_page_"
